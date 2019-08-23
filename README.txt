ae22bc656522935cdc52f797cfa9098f2d279ec7
Adam Abeshouse <adamabeshouse@gmail.com>
2019-08-23 10:45:10 -0400

    Handle dates with no images, and show banner indicating that

--- README.txt
+++ README.txt
@@ -8,3 +8,5 @@ Comments:
 - I decided to go with Vanilla JS, in one file, because it's small enough for now, and because I didn't want to get into setting up a React/Babel/Webpack development environment. I would definitely do it with React/Webpack/Babel if I had more time, because it's more easily maintainable and scalable.
 
 - Another cool UI thing would be to add labels to the range slider, on the left is the time of the earliest image, on the right is the time of the latest image.
+
+- I changed the spec to return an image from the closest date *before* instead of after, because there's no guarantee of finding a closest date after, if their birthday is today and there's no image today.
--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 const API_KEY = "yJtTgrBhiO9zcxu1wL1YF92duSZBIwIHxjStNdrm";
+const LAST_DAY_OF_MONTH = {
+    1: 31,
+    2: 28, // For simplicity, let's not worry about leap years
+    3: 31,
+    4: 30,
+    5: 31,
+    6: 30,
+    7: 31,
+    8: 31,
+    9: 30,
+    10: 31,
+    11: 30,
+    12: 31
+}
 
 class Images {
     constructor(containerEl, imageUrls) {
@@ -49,7 +63,32 @@ class Images {
 
 
 
+function isDateEqual(a, b) {
+    return a.day === b.day && a.month === b.month && a.year === b.year;
+}
 
+function getPrevDay(date) {
+    if (date.day > 1) {
+        // not at first day, so just subtract a day
+        return Object.assign({}, date, { day: date.day-1 });
+    } else if (month > 1) {
+        // were at the first day.
+        // but not at first month, so just go to first day of prev month
+        const prevMonth = date.month - 1;
+        return {
+            month: prevMonth,
+            day: LAST_DAY_OF_MONTH[prevMonth],
+            year: date.year
+        };
+    } else {
+        // we're on january 1st
+        return {
+            month: 12,
+            day: LAST_DAY_OF_MONTH[12],
+            year: date.year - 1
+        };
+    }
+}
 
 function getLastBirthdayYear(monthNum, dayNum) {
     const today = new Date();
@@ -58,8 +97,8 @@ function getLastBirthdayYear(monthNum, dayNum) {
     const currentYear = today.getFullYear();
 
     if (
-            currentMonth < monthNum ||
-            (currentMonth === monthNum && currentDay <= dayNum)
+            monthNum < currentMonth ||
+            (monthNum === currentMonth && dayNum <= currentDay)
         ) {
         return currentYear;
     } else {
@@ -100,12 +139,14 @@ class BearthdayUI {
     constructor(rootEl) {
         this.rootEl = rootEl;
         this.imageUrls = [];
+        this.imageUrlsDate = null;
+        this.birthdayDate = null;
         this.isLoading = false;
         this.parseError = false;
 
         this.rootEl.querySelector("#queryForm").addEventListener("submit", (event)=>{
             event.preventDefault();
-            
+
             this.submitBirthday(rootEl.querySelector("#birthday").value);
         });
 
@@ -114,31 +155,52 @@ class BearthdayUI {
 
     submitBirthday(input) {
         this.imageUrls = [];
+        this.imageUrlsDate = null;
 
         const date = parseBirthday(input);
         if (date) {
+            this.birthdayDate = date;
             this.parseError = false;
             this.isLoading = true;
 
-            // fetch most recent images for date
-            var imagesUrl = `https://api.nasa.gov/EPIC/api/natural/date/${date.year}-${date.month}-${date.day}?api_key=${API_KEY}`;
-            var xmlHttp = new XMLHttpRequest();
-            xmlHttp.onreadystatechange = ()=>{
-                if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-                    const imagesMeta = JSON.parse(xmlHttp.responseText);
-                    this.imageUrls = imagesMeta.map(getImageUrl);
-                    this.isLoading = false;
 
-                    this.render();
-                }
-            }
-            xmlHttp.open("GET", imagesUrl, true);
-            xmlHttp.send(null);
+            this.fetchImageUrls(date, (urls, imageUrlsDate)=>{
+                this.imageUrls = urls;
+                this.isLoading = false;
+                this.imageUrlsDate = imageUrlsDate;
+
+                this.render();
+            });
         } else {
             this.parseError = true;
+            this.birthdayDate = null;
+            this.imageUrlsDate = null;
         }
 
         this.render();
+    }
+
+    fetchImageUrls(date, callback) {
+        // Fetch images for given birthday
+        // If there are none, then try again with the previous day
+        var imagesUrl = `https://api.nasa.gov/EPIC/api/natural/date/${date.year}-${date.month}-${date.day}?api_key=${API_KEY}`;
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.onreadystatechange = ()=>{
+            if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+                const imagesMeta = JSON.parse(xmlHttp.responseText);
+
+                if (imagesMeta.length === 0) {
+                    // no images for this day, so try again with previous day
+                    const prevDay = getPrevDay(date);
+                    this.fetchImageUrls(prevDay, callback);
+                } else {
+                    const imageUrls = imagesMeta.map(getImageUrl);
+                    callback(imageUrls, date);
+                }
+            }
+        }
+        xmlHttp.open("GET", imagesUrl, true);
+        xmlHttp.send(null);
     }
 
     render(){
@@ -162,6 +224,16 @@ class BearthdayUI {
         } else {
             this.rootEl.querySelector("#error").classList.add("hidden");
         }
+
+        // Set b-earth-day banner text
+        let bannerText = "";
+        if (this.imageUrlsDate && this.birthdayDate && isDateEqual(this.imageUrlsDate, this.birthdayDate)) {
+            bannerText = "TRUE B-EARTH-DAY IMAGE!";
+        } else if (this.imageUrlsDate) {
+            bannerText = `Couldn't find images from your most recent birthday. `+
+                        `Images are from closest available: ${this.imageUrlsDate.month}/${this.imageUrlsDate.day}/${this.imageUrlsDate.year}`;
+        }
+        this.rootEl.querySelector("#bearthdayBanner").textContent = bannerText;
 
         const imagesComponent = new Images(imagesArea, this.imageUrls);
     }
